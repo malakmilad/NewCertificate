74f29b7ccaaccacd7f9c518aa6118d4784eb7bbd
Malak Milad <malakmilad202@gmail.com>
2024-06-09 20:04:14 +0300

    dashboard

---
+++ resources/views/admin/layout/scripts.blade.php
@@ -0,0 +1,10 @@
+<script>
+    var hostUrl = "assets/";
+</script>
+<script src="{{ asset('assets/plugins/global/plugins.bundle.js') }}"></script>
+<script src="{{ asset('assets/js/scripts.bundle.js') }}"></script>
+<script src="{{ asset('assets/plugins/custom/fullcalendar/fullcalendar.bundle.js') }}"></script>
+<script src="{{ asset('assets/js/custom/widgets.js') }}"></script>
+<script src="{{ asset('assets/js/custom/apps/chat/chat.js') }}"></script>
+<script src="{{ asset('assets/js/custom/modals/create-app.js') }}"></script>
+<script src="{{ asset('assets/js/custom/modals/upgrade-plan.js') }}"></script>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,231 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html lang="en">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+<head>
+    @include('admin.layout.head')
+</head>
+
+<body id="kt_body"
+    class="header-fixed header-tablet-and-mobile-fixed toolbar-enabled toolbar-fixed aside-enabled aside-fixed"
+    style="--kt-toolbar-height:55px;--kt-toolbar-height-tablet-and-mobile:55px">
+    <div class="d-flex flex-column flex-root">
+        <div class="page d-flex flex-row flex-column-fluid">
+            @include('admin.layout.sidebar')
+            <div class="wrapper d-flex flex-column flex-row-fluid" id="kt_wrapper">
+               @include('admin.layout.navbar')
+                <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+                    <div class="post d-flex flex-column-fluid" id="kt_post">
+                        <div id="kt_content_container" class="container-xxl">
+                      @yield('content')
+                        </div>
+                    </div>
+                </div>
+                @include('admin.layout.footer')
+            </div>
+        </div>
+    </div>
+    <button id="kt_explore_toggle"
+        class="explore-toggle btn btn-sm bg-body btn-color-gray-700 btn-active-primary shadow-sm position-fixed px-5 fw-bolder zindex-2 top-50 mt-10 end-0 transform-90 fs-6 rounded-top-0"
+        title="Explore Metronic" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-trigger="hover">
+        <span id="kt_explore_toggle_label">Explore</span>
+    </button>
+    <div id="kt_explore" class="bg-body" data-kt-drawer="true" data-kt-drawer-name="explore"
+        data-kt-drawer-activate="true" data-kt-drawer-overlay="true"
+        data-kt-drawer-width="{default:'350px', 'lg': '475px'}" data-kt-drawer-direction="end"
+        data-kt-drawer-toggle="#kt_explore_toggle" data-kt-drawer-close="#kt_explore_close">
+        <div class="card shadow-none rounded-0 w-100">
+            <div class="card-header" id="kt_explore_header">
+                <h3 class="card-title fw-bolder text-gray-700">Explore Metronic</h3>
+                <div class="card-toolbar">
+                    <button type="button" class="btn btn-sm btn-icon btn-active-light-primary me-n5"
+                        id="kt_explore_close">
+                        <span class="svg-icon svg-icon-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                viewBox="0 0 24 24" fill="none">
+                                <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1"
+                                    transform="rotate(-45 6 17.3137)" fill="black" />
+                                <rect x="7.41422" y="6" width="16" height="2" rx="1"
+                                    transform="rotate(45 7.41422 6)" fill="black" />
+                            </svg>
+                        </span>
+                    </button>
+                </div>
+            </div>
+            <div class="card-body" id="kt_explore_body">
+                <div id="kt_explore_scroll" class="scroll-y me-n5 pe-5" data-kt-scroll="true"
+                    data-kt-scroll-height="auto" data-kt-scroll-wrappers="#kt_explore_body"
+                    data-kt-scroll-dependencies="#kt_explore_header" data-kt-scroll-offset="5px">
+                    <div class="mb-0">
+                        <div class="mb-7">
+                            <div class="d-flex flex-stack">
+                                <h3 class="mb-0">Metronic Licenses</h3>
+                                <a href="https://themeforest.net/licenses/standard" class="fw-bold"
+                                    target="_blank">License FAQs</a>
+                            </div>
+                        </div>
+                        <div class="rounded border border-dashed border-gray-300 py-4 px-6 mb-5">
+                            <div class="d-flex flex-stack">
+                                <div class="d-flex flex-column">
+                                    <div class="d-flex align-items-center mb-1">
+                                        <div class="fs-6 fw-bold text-gray-800 fw-bold mb-0 me-1">Regular License
+                                        </div>
+                                        <i class="text-gray-400 fas fa-exclamation-circle ms-1 fs-7"
+                                            data-bs-toggle="popover" data-bs-custom-class="popover-dark"
+                                            data-bs-trigger="hover" data-bs-placement="top"
+                                            data-bs-content="Use, by you or one client in a single end product which end users are not charged for."></i>
+                                    </div>
+                                    <div class="fs-7 text-muted">For single end product used by you or one client
+                                    </div>
+                                </div>
+                                <div class="text-nowrap">
+                                    <span class="text-muted fs-7 fw-bold me-n1">$</span>
+                                    <span class="text-dark fs-1 fw-bolder">39</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="rounded border border-dashed border-gray-300 py-4 px-6 mb-5">
+                            <div class="d-flex flex-stack">
+                                <div class="d-flex flex-column">
+                                    <div class="d-flex align-items-center mb-1">
+                                        <div class="fs-6 fw-bold text-gray-800 fw-bold mb-0 me-1">Extended License
+                                        </div>
+                                        <i class="text-gray-400 fas fa-exclamation-circle ms-1 fs-7"
+                                            data-bs-toggle="popover" data-bs-custom-class="popover-dark"
+                                            data-bs-trigger="hover" data-bs-placement="top"
+                                            data-bs-content="Use, by you or one client, in a single end product which end users can be charged for."></i>
+                                    </div>
+                                    <div class="fs-7 text-muted">For single end product with paying users.</div>
+                                </div>
+                                <div class="text-nowrap">
+                                    <span class="text-muted fs-7 fw-bold me-n1">$</span>
+                                    <span class="text-dark fs-1 fw-bolder">939</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="rounded border border-dashed border-gray-300 py-4 px-6 mb-5">
+                            <div class="d-flex flex-stack">
+                                <div class="d-flex flex-column">
+                                    <div class="d-flex align-items-center mb-1">
+                                        <div class="fs-6 fw-bold text-gray-800 fw-bold mb-0 me-1">Custom License</div>
+                                    </div>
+                                    <div class="fs-7 text-muted">Reach us for custom license offers.</div>
+                                </div>
+                                <div class="text-nowrap">
+                                    <a href="https://keenthemes.com/contact/" class="btn btn-sm btn-success"
+                                        target="_blank">Contact Us</a>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="https://1.envato.market/EA4JP" class="btn btn-primary mb-15 w-100">Buy Now</a>
+                        <div class="mb-0">
+                            <h3 class="fw-bolder text-center mb-6">Metronic Demos</h3>
+                            <div class="row g-5">
+                                <div class="col-6">
+                                    <!--begin::Demo-->
+                                    <div
+                                        class="overlay overflow-hidden position-relative border border-4 border-success rounded">
+                                        <div class="overlay-wrapper">
+                                            <img src="assets/media/demos/demo1.png" alt="demo" class="w-100" />
+                                        </div>
+                                        <div class="overlay-layer bg-dark bg-opacity-10">
+                                            <a href="https://preview.keenthemes.com/metronic8/demo1"
+                                                class="btn btn-sm btn-success shadow">Demo 1</a>
+                                        </div>
+                                    </div>
+                                    <!--end::Demo-->
+                                </div>
+                                <div class="col-6">
+                                    <!--begin::Demo-->
+                                    <div
+                                        class="overlay overflow-hidden position-relative border border-4 border-gray-200 rounded">
+                                        <div class="overlay-wrapper">
+                                            <img src="assets/media/demos/demo2.png" alt="demo" class="w-100" />
+                                        </div>
+                                        <div class="overlay-layer bg-dark bg-opacity-10">
+                                            <a href="https://preview.keenthemes.com/metronic8/demo2"
+                                                class="btn btn-sm btn-success shadow">Demo 2</a>
+                                        </div>
+                                    </div>
+                                    <!--end::Demo-->
+                                </div>
+                                <div class="col-6">
+                                    <!--begin::Demo-->
+                                    <div
+                                        class="overlay overflow-hidden position-relative border border-4 border-gray-200 rounded">
+                                        <div class="overlay-wrapper">
+                                            <img src="assets/media/demos/demo14.png" alt="demo"
+                                                class="w-100 opacity-25" />
+                                        </div>
+                                        <div class="overlay-layer bg-dark bg-opacity-10">
+                                            <div class="badge badge-white px-6 py-4 fw-bold fs-base shadow">Coming
+                                                soon</div>
+                                        </div>
+                                    </div>
+                                    <!--end::Demo-->
+                                </div>
+                                <div class="col-6">
+                                    <!--begin::Demo-->
+                                    <div
+                                        class="overlay overflow-hidden position-relative border border-4 border-gray-200 rounded">
+                                        <div class="overlay-wrapper">
+                                            <img src="assets/media/demos/demo15.png" alt="demo"
+                                                class="w-100 opacity-25" />
+                                        </div>
+                                        <div class="overlay-layer bg-dark bg-opacity-10">
+                                            <div class="badge badge-white px-6 py-4 fw-bold fs-base shadow">Coming
+                                                soon</div>
+                                        </div>
+                                    </div>
+                                    <!--end::Demo-->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</x-app-layout>
+    <div class="modal fade" id="kt_modal_invite_friends" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog mw-650px">
+            <div class="modal-content">
+                <div class="modal-header pb-0 border-0 justify-content-end">
+                    <div class="btn btn-sm btn-icon btn-active-color-primary" data-bs-dismiss="modal">
+                        <span class="svg-icon svg-icon-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                viewBox="0 0 24 24" fill="none">
+                                <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1"
+                                    transform="rotate(-45 6 17.3137)" fill="black" />
+                                <rect x="7.41422" y="6" width="16" height="2" rx="1"
+                                    transform="rotate(45 7.41422 6)" fill="black" />
+                            </svg>
+                        </span>
+                    </div>
+                </div>
+                <div class="modal-body scroll-y mx-5 mx-xl-18 pt-0 pb-15">
+                    <div class="text-center mb-13">
+                        <h1 class="mb-3">Invite a Friend</h1>
+                    </div>
+                    <div class="separator d-flex flex-center mb-8">
+                        <span class="text-uppercase bg-body fs-7 fw-bold text-muted px-3">or</span>
+                    </div>
+                    <textarea class="form-control form-control-solid mb-8" rows="3" placeholder="Type or paste emails here"></textarea>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="kt_scrolltop" class="scrolltop" data-kt-scrolltop="true">
+        <span class="svg-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                fill="none">
+                <rect opacity="0.5" x="13" y="6" width="13" height="2" rx="1"
+                    transform="rotate(90 13 6)" fill="black" />
+                <path
+                    d="M12.5657 8.56569L16.75 12.75C17.1642 13.1642 17.8358 13.1642 18.25 12.75C18.6642 12.3358 18.6642 11.6642 18.25 11.25L12.7071 5.70711C12.3166 5.31658 11.6834 5.31658 11.2929 5.70711L5.75 11.25C5.33579 11.6642 5.33579 12.3358 5.75 12.75C6.16421 13.1642 6.83579 13.1642 7.25 12.75L11.4343 8.56569C11.7467 8.25327 12.2533 8.25327 12.5657 8.56569Z"
+                    fill="black" />
+            </svg>
+        </span>
+    </div>
+    @include('admin.layout.scripts')
+</body>
+
+</html>
