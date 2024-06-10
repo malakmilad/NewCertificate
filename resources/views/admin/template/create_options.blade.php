fbdc2e490ecad42ac5d6f3c9946bb59d03d75d4a
Malak Milad <malakmilad202@gmail.com>
2024-06-10 22:53:12 +0300

    template create ui

---
+++ resources/views/admin/template/create_options.blade.php
@@ -0,0 +1,111 @@
+<button id="kt_explore_toggle"
+    class="explore-toggle btn btn-sm bg-body btn-color-gray-700 btn-active-primary shadow-sm position-fixed px-5 fw-bolder zindex-2 top-50 mt-10 end-0 transform-90 fs-6 rounded-top-0"
+    title="Explore Options" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-trigger="hover">
+    <span id="kt_explore_toggle_label">Options</span>
+</button>
+<div id="kt_explore" class="bg-body" data-kt-drawer="true" data-kt-drawer-name="explore" data-kt-drawer-activate="true"
+    data-kt-drawer-overlay="false" data-kt-drawer-width="{default:'350px', 'lg': '475px'}"
+    data-kt-drawer-direction="end" data-kt-drawer-toggle="#kt_explore_toggle" data-kt-drawer-close="#kt_explore_close">
+    <div class="card shadow-none rounded-0 w-100">
+        <div class="card-header" id="kt_explore_header">
+            <h3 class="card-title fw-bolder text-gray-700">Explore Options</h3>
+            <div class="card-toolbar">
+                <button type="button" class="btn btn-sm btn-icon btn-active-light-primary me-n5" id="kt_explore_close">
+                    <span class="svg-icon svg-icon-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            fill="none">
+                            <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1"
+                                transform="rotate(-45 6 17.3137)" fill="black" />
+                            <rect x="7.41422" y="6" width="16" height="2" rx="1"
+                                transform="rotate(45 7.41422 6)" fill="black" />
+                        </svg>
+                    </span>
+                </button>
+            </div>
+        </div>
+        <div class="card-body" id="kt_explore_body">
+            <div id="kt_explore_scroll" class="scroll-y me-n5 pe-5" data-kt-scroll="true" data-kt-scroll-height="auto"
+                data-kt-scroll-wrappers="#kt_explore_body" data-kt-scroll-dependencies="#kt_explore_header"
+                data-kt-scroll-offset="5px">
+                <div class="mb-0">
+                    <div class="rounded border border-dashed border-gray-300 py-4 px-6 mb-5">
+                        <div>
+                            <label class="form-label">test</label>
+                            <input type="text" name="" id="" class="form-control">
+                        </div>
+                    </div>
+                    <div class="rounded border border-dashed border-gray-300 py-4 px-6 mb-5">
+                        <div>
+                            <label class="form-label">test</label>
+                            <input type="text" name="" id="" class="form-control">
+                        </div>
+                    </div>
+                    <div class="mb-5">
+                        <h3 class="fw-bolder text-center mb-6" style="color: #C70815;">
+                            Student</h3>
+                        <div class="row g-5">
+                            <div class="col-6">
+                                <label class="form-label">test</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="col-6">
+                                <label class="form-label">test</label>
+                                <input type="text" class="form-control" name="" id="">
+                            </div>
+                            <div class="col-6">
+                                <label class="form-label">test</label>
+                                <input type="text" class="form-control" name="" id="">
+                            </div>
+                            <div class="col-6">
+                                <label class="form-label">test</label>
+                                <input type="text" class="form-control" name="" id="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mb-5">
+                        <h3 class="fw-bolder text-center mb-6" style="color: #C70815;">Course</h3>
+                        <div class="row g-5">
+                            <div class="col-6">
+                                <label class="form-label">test</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="col-6">
+                                <label class="form-label">test</label>
+                                <input type="text" class="form-control" name="" id="">
+                            </div>
+                            <div class="col-6">
+                                <label class="form-label">test</label>
+                                <input type="text" class="form-control" name="" id="">
+                            </div>
+                            <div class="col-6">
+                                <label class="form-label">test</label>
+                                <input type="text" class="form-control" name="" id="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mb-5">
+                        <h3 class="fw-bolder text-center mb-6" style="color: #C70815;">Date</h3>
+                        <div class="row g-5">
+                            <div class="col-6">
+                                <label class="form-label">test</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="col-6">
+                                <label class="form-label">test</label>
+                                <input type="text" class="form-control" name="" id="">
+                            </div>
+                            <div class="col-6">
+                                <label class="form-label">test</label>
+                                <input type="text" class="form-control" name="" id="">
+                            </div>
+                            <div class="col-6">
+                                <label class="form-label">test</label>
+                                <input type="text" class="form-control" name="" id="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
