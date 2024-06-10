3a8dde2619216c2279371c93d185566bfb22b796
Malak Milad <malakmilad202@gmail.com>
2024-06-10 21:45:22 +0300

    remove unused controller

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,33 +1,3 @@
-{{-- <x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
-                autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
-                autocomplete="current-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout> --}}
 <!DOCTYPE html>
 <html lang="en">
 
@@ -37,40 +7,41 @@
 
 <body id="kt_body" class="bg-body">
     <div class="d-flex flex-column flex-root">
-        <div class="d-flex flex-column flex-column-fluid bgi-position-y-bottom position-x-center bgi-no-repeat bgi-size-contain bgi-attachment-fixed"
-            style="background-image: url(assets/media/illustrations/sketchy-1/14.png">
+        <div
+            class="d-flex flex-column flex-column-fluid bgi-position-y-bottom position-x-center bgi-no-repeat bgi-size-contain bgi-attachment-fixed">
             <div class="d-flex flex-center flex-column flex-column-fluid p-10 pb-lg-20">
                 <div class="mb-12">
                     <img alt="Logo" src="{{ asset('assets/media/logos/logo.png') }}" class="h-40px" />
                 </div>
                 <div class="w-lg-500px bg-body rounded shadow-sm p-10 p-lg-15 mx-auto">
-                    <form class="form w-100" novalidate="novalidate" id="kt_sign_in_form" action="#">
+                    <form class="form w-100" novalidate="novalidate" id="kt_sign_in_form" action="{{ route('login') }}"
+                        method="POST">
+                        @csrf
                         <div class="text-center mb-10">
                             <h1 class="text-dark mb-3">Sign In to EEIC</h1>
                         </div>
                         <div class="fv-row mb-10">
-                            <label class="form-label fs-6 fw-bolder text-dark">Email</label>
-                            <input class="form-control form-control-lg form-control-solid" type="text" name="email"
-                                autocomplete="off" />
+                            <x-input-label for="email" :value="__('Email')" />
+                            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email"
+                                :value="old('email')" required autofocus autocomplete="username" />
+                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
                         </div>
                         <div class="fv-row mb-10">
                             <div class="d-flex flex-stack mb-2">
-                                <label class="form-label fw-bolder text-dark fs-6 mb-0">Password</label>
-
+                                <x-input-label for="password" :value="__('Password')" />
                             </div>
-
-                            <input class="form-control form-control-lg form-control-solid" type="password"
-                                name="password" autocomplete="off" />
+                            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password"
+                                required autocomplete="current-password" />
+                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
                         </div>
                         <div class="text-center">
                             <button type="submit" id="kt_sign_in_submit" class="btn btn-lg btn-primary w-100 mb-5">
-                                <span class="indicator-label">Continue</span>
+                                <span class="indicator-label">Login</span>
                             </button>
                         </div>
                     </form>
                 </div>
             </div>
-
         </div>
     </div>
     @include('admin.layout.scripts')
--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,7 +1,7 @@
 @props(['messages'])
 
 @if ($messages)
-    <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
+    <ul {{ $attributes->merge(['class' => 'alert alert-dismissible bg-light-danger border border-danger border-dashed d-flex flex-column flex-sm-row w-100 p-5 mb-5 mt-5']) }}>
         @foreach ((array) $messages as $message)
             <li>{{ $message }}</li>
         @endforeach
