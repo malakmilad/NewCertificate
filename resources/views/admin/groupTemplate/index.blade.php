4e2e2494ddbd2512ab66a1168237610dee138001
Malak Milad <malakmilad202@gmail.com>
2024-06-11 21:53:59 +0300

    Datatable

--- resources/views/admin/groupTemplate/index.blade.php
+++ resources/views/admin/groupTemplate/index.blade.php
@@ -6,7 +6,7 @@
                 <div class="card-header border-0 pt-5">
                     <h3 class="card-title align-items-start flex-column">
                         <span class="card-label fw-bolder fs-3 mb-1">Student</span>
-                        <span class="text-muted mt-1 fw-bold fs-7">Over</span>
+                        <span class="text-muted mt-1 fw-bold fs-7">Over :{{count($students)}}</span>
                     </h3>
                     <div class="card-toolbar" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-trigger="hover"
                         title="Click to add a Generate">
@@ -26,7 +26,7 @@
                 </div>
                 <div class="card-body py-3">
                     <div class="table-responsive">
-                        <table class="table table-row-dashed table-row-gray-300 align-middle gs-0 gy-4">
+                        <table id="generate" class="table table-row-dashed table-row-gray-300 align-middle gs-0 gy-4">
                             <thead>
                                 <tr class="fw-bolder text-muted">
                                     <th class="w-25px">ID</th>
@@ -40,25 +40,36 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach($students as $student)
-                                <tr>
-                                    <td>{{ $student['id'] }}</td>
-                                    <td>{{ $student['name'] }}</td>
-                                    <td>{{ $student['email'] }}</td>
-                                    <td>{{ $student['uuid'] }}</td>
-                                    <td>{{ $student['phone'] }}</td>
-                                    <td>
-                                            @foreach($student['courses'] as $course)
-                                            <span class="badge badge-light-danger">{{ $course }}</span>
-
+                                @foreach ($students as $student)
+                                    <tr>
+                                        <td>{{ $student['id'] }}</td>
+                                        <td>{{ $student['name'] }}</td>
+                                        <td>{{ $student['email'] }}</td>
+                                        <td>{{ $student['uuid'] }}</td>
+                                        <td>{{ $student['phone'] }}</td>
+                                        <td>
+                                            @foreach ($student['courses'] as $course)
+                                                <span class="badge badge-light-danger">{{ $course }}</span>
                                             @endforeach
-                                    </td>
-                                    <td>{{ $student['template'] }}</td>
-                                    <td class="text-end">
-                                        <button class="btn btn-sm btn-primary">Edit</button>
-                                    </td>
-                                </tr>
-                            @endforeach
+                                        </td>
+                                        <td>{{ $student['template'] }}</td>
+                                        <td>
+                                            <div class="d-flex justify-content-end flex-shrink-0">
+                                                <a href=""
+                                                    class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm">
+                                                    <span class="svg-icon svg-icon-3">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                        fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+                                                        <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0" />
+                                                        <path
+                                                            d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7" />
+                                                    </svg>
+                                                    </span>
+                                                </a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
@@ -68,3 +79,8 @@
     </div>
     @include('admin.groupTemplate.create')
 @endsection
+@push('script')
+<script>
+    new DataTable('#generate');
+</script>
+@endpush
